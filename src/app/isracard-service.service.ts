f068bf9d4c6d75c9d9bc61b3c70b801e27b81e25
elimail2010 <elimail2010@gmail.com>
2019-12-31 18:11:00 +0200

    isracard-v2.00
    testing github webpages

--- src/app/isracard-service.service.ts
+++ src/app/isracard-service.service.ts
@@ -25,7 +25,7 @@ export class IsracardServiceService {
   getRepos(term:string){
     //dev url:  'http://localhost:10741/api/github?q='
     //prod url :'http://webappisracard-dev.us-west-2.elasticbeanstalk.com/api/github?q='
-    return this._http.get<IRepo>( 'http://webappisracard-dev.us-west-2.elasticbeanstalk.com/api/github?q='+term)
+    return this._http.get<IRepo>( 'http://isracardhub-env.s6ajhnqvhm.us-east-1.elasticbeanstalk.com/WebApp-isracard_deploy/api/GitHub?q='+term)
     .do((res:any)=>console.log('get items service donbe'))
     .map((res:any)=>res.reposResult.items) 
     .catch((err) => {
